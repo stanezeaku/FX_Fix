7e5c6047512e1275bbad4e0fe8a3e77c729be800
Ezeaku Stanley <ifeetech@gmail.com>
2020-05-16 05:01:31 -0700

    Update

--- app/src/main/java/com/ifeetech/fxfix/ui/tradesessions/TradeSessionsFragment.kt
+++ app/src/main/java/com/ifeetech/fxfix/ui/tradesessions/TradeSessionsFragment.kt
@@ -6,11 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.ifeetech.fxfix.R
 import com.ifeetech.fxfix.databinding.FragmentTradeSessionsBinding
+import kotlinx.android.synthetic.main.board_trade_session.view.*
 
 class TradeSessionsFragment : Fragment() {
 
 
+    private lateinit var binding: FragmentTradeSessionsBinding
     private val model: TradeSessionsViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -19,15 +22,40 @@ class TradeSessionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding = FragmentTradeSessionsBinding.inflate(inflater, container, false)
+        binding = FragmentTradeSessionsBinding.inflate(inflater, container, false)
 
 
         binding.homeViewModel = model
 
         binding.lifecycleOwner = this
 
+        setSydneySession(binding)
+        setTokyoSession(binding)
+        setLondonSession(binding)
+        setNewYorkSession(binding)
+
 
 
         return binding.root
+    }
+
+    private fun setSydneySession(binding: FragmentTradeSessionsBinding) {
+        binding.includeSydneySession.image_one.setImageResource(R.drawable.ic_aus_flag)
+
+    }
+
+    private fun setTokyoSession(binding: FragmentTradeSessionsBinding) {
+        binding.includeTokyoSession.image_one.setImageResource(R.drawable.ic_japan_flag)
+
+    }
+
+    private fun setLondonSession(binding: FragmentTradeSessionsBinding) {
+        binding.includeLondonSession.image_one.setImageResource(R.drawable.ic_uk_flag)
+
+    }
+
+    private fun setNewYorkSession(binding: FragmentTradeSessionsBinding) {
+        binding.includeNewyorkSession.image_one.setImageResource(R.drawable.ic_usa_flag)
+
     }
 }
